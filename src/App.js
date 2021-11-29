3cc31d54956bbb8c121a3d5b4223525574a1cbfe
Aleksandra <arteg132579@gmail.com>
2021-11-29 21:01:30 +0300

    Create CategoryPage; implement query to GraphQl

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header.js'
+import CategoryPage from './components/CategoryPage.js'
 import React from 'react';
 import {gql} from "@apollo/client";
 
@@ -10,11 +11,53 @@ class App extends React.Component {
     this.state = {
       categoryName:'',
       products: [],
+      itemIsClicked: false,
       homePageIconIsClicked: true,
       headerCartIconIsClicked: false,
+      choosenProduct: '',
+      cart: {
+        products: [],
+        total: 0,
+        amountOfItems: 0
+      }
     }
     this.handleHomePageClick = this.handleHomePageClick.bind(this);
     this.handleHeaderCartClick = this.handleHeaderCartClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.client
+    .query({
+      query: gql`
+        query {
+          categories {
+            name
+            products {
+              id,
+              name,
+              inStock,
+              gallery,
+              prices {
+                currency,
+                amount
+              },
+              description,
+              attributes {
+                name,
+                items {
+                  value
+                }
+              }
+            }
+          }
+        }`
+    })
+    .then((result) => {
+      this.setState({
+        categoryName: result.data.categories[1].name,
+        products: result.data.categories[1].products
+      })
+    });
   }
 
   handleHomePageClick() {
@@ -36,6 +79,9 @@ class App extends React.Component {
         <Header
           handleHomePageClick={this.handleHomePageClick}
           handleHeaderCartClick={this.handleHeaderCartClick}/>
+        <CategoryPage
+          categoryName={this.state.categoryName}
+          products={this.state.products}/>
       </div>
     );
   }
---
+++ src/components/CategoryPage.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class CategoryPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isCardHovered : false
+    }
+    this.createCardElement = this.createCardElement.bind(this);
+  }
+
+  createCardElement(product) {
+    let warningAboutUnstockedItem, unstokedItemStyle;
+    if (!product.inStock) {
+      warningAboutUnstockedItem = <span className="unstoked-warning">out of stock</span>;
+      unstokedItemStyle = {opacity: "0.5"};
+    }
+    return (
+      <div
+        key={product.id}
+        id={product.id}
+        className="product-card"
+        onClick={this.props.handleProductClick}
+      >
+        {warningAboutUnstockedItem}
+        <img className="product-image" src={product.gallery[0]} style={unstokedItemStyle} alt=""></img>
+        <span className="product-name" style={unstokedItemStyle}>{product.name}</span>
+        <span className="product-price" style={unstokedItemStyle}>{product.prices[0].amount}{product.prices[0].currency}</span>
+        {this.state.isCardHovered && (
+          <div className="category-page__add-to-card-button">
+            <img src="/images/white-cart.svg" alt="cart-icon"></img>
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  render() {
+    const productsArray = this.props.products;
+    const listProducts = productsArray.map((product) => this.createCardElement(product));
+    return (
+      <div className="category-page">
+        <h1>{this.props.categoryName}</h1>
+        <div className="products-wrapper">
+          {listProducts}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CategoryPage
